f70dd6066d70ac587e3ad34c639d1f695f212aa4
zzw <306505258@qq.com>
2020-08-20 23:09:28 +0800

    按钮5

--- lib/demo/chip_demo.dart
+++ lib/demo/chip_demo.dart
@@ -6,6 +6,14 @@ class ChipDemo extends StatefulWidget {
 }
 
 class _ChipDemoState extends State<ChipDemo> {
+  List<String> _tags = [
+    'Apple',
+    'Banana',
+    'Lemon'
+  ];
+
+  List<String> _selected = [];
+  String _action = 'Nothing';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,13 +26,16 @@ class _ChipDemoState extends State<ChipDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            //超出屏幕会另起一行显示，本身可以设置间隔
+            Wrap(
+              // 小部件之间的间隔
+              spacing: 8.0,
+              // 行与行之间的间隔
+              runSpacing: 8.0,
               children: <Widget>[
                 Chip(
                   label: Text('Life'),
                 ),
-                SizedBox(width: 8.0),
                 Chip(
                   label: Text('Sunset'),
                   backgroundColor: Colors.orange,
@@ -42,10 +53,107 @@ class _ChipDemoState extends State<ChipDemo> {
                       backgroundImage: NetworkImage('https://resources.ninghao.org/images/gravity-falls.png'),
                     ),
                 ),
+                Chip(
+                  label: Text('City'),
+                  //按下删除按钮实现这个方法
+                  onDeleted: (){},
+                  deleteIcon: Icon(Icons.delete),
+                  deleteIconColor: Colors.redAccent,
+                  //长按小图标出现的提示
+                  deleteButtonTooltipMessage: 'Remove this tag',
+                ),
+                // 分隔符
+                Divider(
+                  color: Colors.grey,
+                  height: 32.0,
+                  //缩进
+                  indent: 32.0,
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  // map方法迭代处理_tags的项目，并转为list
+                  children: _tags.map((tag){
+                    return Chip(
+                      label: Text(tag),
+                      onDeleted: (){
+                        setState(() {
+                          _tags.remove(tag);
+                        });
+                      },
+                    );
+                  }).toList()
+                ),
+                Divider(
+                  color: Colors.grey,
+                  height: 32.0,
+                  //缩进
+                  indent: 32.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text('FilterChip: ${_selected.toString()}'),
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  // map方法迭代处理_tags的项目，并转为list
+                  children: _tags.map((tag){
+                    // filterChip有选中和未选中两种状态
+                    return FilterChip(
+                      label: Text(tag),
+                      // 选中的状态，t or f, 如果_selected包含项目就是被选中的状态
+                      selected: _selected.contains(tag),
+                      onSelected: (value){
+                        setState(() {
+                          if(_selected.contains(tag)){
+                            // 在选中的项目中去掉当前的项目
+                            _selected.remove(tag);
+                          }else{
+                            _selected.add(tag);
+                          }
+                        });
+                      },
+                    );
+                  }).toList()
+                ),
+                Divider(
+                  color: Colors.grey,
+                  height: 32.0,
+                  //缩进
+                  indent: 32.0,
+                ),
+                Wrap(
+                    spacing: 8.0,
+                    // map方法迭代处理_tags的项目，并转为list
+                    children: _tags.map((tag){
+                      return Chip(
+                        label: Text(tag),
+                        onDeleted: (){
+                          setState(() {
+                            _tags.remove(tag);
+                          });
+                        },
+                      );
+                    }).toList()
+                ),
               ],
             ),
           ],
         ),
+      ),
+      // 重置功能
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.restore),
+        onPressed: (){
+          setState(() {
+            _tags = [
+              'Apple',
+              'Banana',
+              'Lemon',
+            ];
+            // 点击按钮成为重置选择状态
+            _selected = [];
+          });
+        },
       ),
     );
   }
